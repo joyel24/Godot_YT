6368be32751de29270e57014f814c1084f81ef61
joel <joel.martin.2104@gmail.com>
2024-04-30 16:46:45 +0200

    http req every 60 frames when stopped on sign

--- godot/World/World.gd
+++ godot/World/World.gd
@@ -3,19 +3,29 @@ extends Node2D
 onready var transition = $UI/Transition
 onready var tilemap = $WorldMap
 
+var requests_ratio = 0
+
 func _ready():
 	Global.text_box = ""
 	calculate_switch_blocks()
 	var _error = Global.connect("blocks_switched",self,"calculate_switch_blocks")
 	transition.open()
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	
-func _physics_process(delta):
+func _physics_process(delta): #process every frame
 	#print(Global.on_sign_lbl)
 	if Global.on_sign_lbl == false: #walk right if not on sign
 		Global.walk = 1 #cf player.gd
 	elif Global.on_sign_lbl == true: #if sign: stop walk
 		Global.walk = 0
+		requests_ratio = requests_ratio+1
+		if requests_ratio >= 59: #runs every 60 frames on sign stopped
+			$HTTPRequest.request("http://127.0.0.1:8080/index.html")
+			requests_ratio = 0
 		
+func _on_request_completed(result, response_code, headers, body): #$HTTPRequest
+	if response_code == 200:
+		print(body.get_string_from_utf8())
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("pause"):
