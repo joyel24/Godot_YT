cb0837791b7e2daa743cc8aeae7e70dca562873b
joel <joel.martin.2104@gmail.com>
2024-04-30 21:09:19 +0200

    menu: start game from http

--- godot/UI/Menu.gd
+++ godot/UI/Menu.gd
@@ -2,10 +2,22 @@ extends CanvasLayer
 
 onready var transition = $Transition
 
+var requests_ratio = 0
+
 func _ready():
 	transition.open(1.0,0.25)
-	get_tree().change_scene("res://World/World.tscn") #Run World...
+	#get_tree().change_scene("res://World/World.tscn") #Run World without menu...
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	
+func _physics_process(delta):
+	requests_ratio += 1
+	if requests_ratio >= 59:
+		$HTTPRequest.request("http://127.0.0.1:8080/index.html?&started")
+		requests_ratio = 0
+
+func _on_request_completed(result, response_code, headers, body):
+	if response_code == 299: #START GAME if http response code is 299
+		_on_Load_timeout();
 
 func _on_Play_button_down():
 	transition.close(1.0)
